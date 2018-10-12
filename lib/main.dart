0c5ca567fcc2b4ecbb1c026ba134d5c7ba4efc7b
Andrii Mutylo <developer@Andriis-Air.Dlink>
2018-10-12 22:04:16 +0300

    fix typo

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,8 @@ void main() => runApp(MyApp());
 
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
+  
+  build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
